3df560070e492df9030d10f1bf9a43d45bb18e60
clauherediaz <51749091+clauherediaz@users.noreply.github.com>
2019-06-22 21:15:56 -0400

    Add files via upload

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,5 +1,6 @@
-describe('cipher', () => {
-
+//describe a quien voy a testear 
+describe('cipher.js', () => {
+//que voy a testear 
   it('debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
   });
@@ -7,19 +8,27 @@ describe('cipher', () => {
   describe('cipher.encode', () => {
 
     it('debería ser una función', () => {
+      //asserte es mi comparacion 
       assert.equal(typeof cipher.encode, 'function');
     });
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
-  });
+    assert.equal(window.cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
 
+  });
+ 
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', ()=> {
+    assert.equal(window.cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    });
+
   });
 
 });
+
