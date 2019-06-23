44c5a7d44470ce32f166e3728a807f6db9b0d492
clauherediaz <51749091+clauherediaz@users.noreply.github.com>
2019-06-22 21:15:11 -0400

    Add files via upload

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,44 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+window.cipher = {   // inicio de función
+  
+  encode: (textoIngresado, desplazar) => {   // sección de cifrar datos ( encode )
     
+    let cifradoAscci = "";    
+
+    for (let i = 0; i <= textoIngresado.length; i++) {  
+
+      let cesar = textoIngresado.charCodeAt(i);   
+                                                
+
+      if (cesar >= 65 && cesar <= 90) {   // 65 y 90 son los valores en que ronda el abecedario en asccii
+
+             cifradoAscci += String.fromCharCode(((cesar - 65 + desplazar) % 26) + 65);   // cálculos de algoritmo universal para código cesar
+      } else if ((cesar >= 97) && (cesar <= 122)) {
+        cifradoAscci += String.fromCharCode(((cesar - 97 + (desplazar)) % 26) + 97);
+      } 
+
+    }
+
+    return cifradoAscci; 
+                            
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (textoIngresado, desplazar) => {    
+
+    let cifradoAscci = '';
+
+
+    for (let i = 0; i <= textoIngresado.length; i++) {
+      let cesar = textoIngresado.charCodeAt(i);
+      if ((cesar >= 65) && (cesar <= 90)) {
+        cifradoAscci += String.fromCharCode(((cesar - 90 - desplazar) % 26) + 90);
+        //cifradoEnAscci es igual a vacio + el cifrado 
+      } else if ((cesar >= 97) && (cesar <= 122)) {
+        cifradoAscci += String.fromCharCode(((cesar - 122 - desplazar) % 26) + 122);
+      }
+
+    }
+
+    return cifradoAscci;
+    
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-/*Acá va tu código*/
+   document.getElementById("cifrar").addEventListener("click", function () {   
+  
+
+
+    let textoIngresado = document.getElementById('key1').value;   
+
+    
+                    
+    let desplazar = parseInt(document.getElementById('offset1').value);
+
+     
+    document.getElementById('textCipher').value += cipher.encode(textoIngresado, desplazar);  
+  
+  })    
+   document.getElementById("decifrar").addEventListener("click", function () {   
+    let textoIngresado = document.getElementById('key1').value;   
+    let desplazar = parseInt(document.getElementById('offset1').value); 
+  
+  
+    document.getElementById('textCipher').value += cipher.decode(textoIngresado, desplazar);  
+  
+  });
